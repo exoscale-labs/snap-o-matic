6114819975780238dccc09d3f420881795a9b179
Marc Falzon <marc@exoscale.com>
2019-10-18 18:44:43 +0200

    Initial commit ✨

---
+++ go.mod
@@ -0,0 +1,13 @@
+module snap-o-matic
+
+go 1.13
+
+require (
+	github.com/caarlos0/env/v6 v6.1.0
+	github.com/exoscale/egoscale v0.18.1
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
+	github.com/spf13/pflag v1.0.5
+	gopkg.in/inconshreveable/log15.v2 v2.0.0-20180818164646-67afb5ed74ec
+)
---
+++ main.go
@@ -0,0 +1,260 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log/syslog"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/caarlos0/env/v6"
+	"github.com/exoscale/egoscale"
+	flag "github.com/spf13/pflag"
+	log "gopkg.in/inconshreveable/log15.v2"
+)
+
+// TODO:
+// - support reading API credentials from a file
+
+const (
+	metadataURL = "http://169.254.169.254/latest/meta-data/vm-id"
+	autosnapTag = "autosnap"
+
+	defaultSnapshotsRetention = 7
+)
+
+var (
+	exo *egoscale.Client
+	cfg config
+)
+
+type config struct {
+	APIEndpoint        string `env:"EXOSCALE_API_ENDPOINT" envDefault:"https://api.exoscale.com/compute"`
+	APIKey             string `env:"EXOSCALE_API_KEY,required"`
+	APISecret          string `env:"EXOSCALE_API_SECRET,required"`
+	SnapshotsRetention int
+	DryRun             bool
+	InstanceID         string
+}
+
+func init() {
+	var (
+		logTo      string
+		logLevel   string
+		logHandler log.Handler
+		err        error
+	)
+
+	if err := env.Parse(&cfg); err != nil {
+		dieOnError("initialization failed", "error", err)
+	}
+
+	flag.IntVarP(&cfg.SnapshotsRetention, "snapshot-retention", "r", defaultSnapshotsRetention,
+		"Maximum snapshots retention")
+	flag.StringVarP(&cfg.InstanceID, "instance-id", "i", "",
+		"ID of the instance to snapshot (disables instance-local lookup)")
+	flag.StringVarP(&logTo, "log", "l", "-",
+		`File to log activity to, "-" to log to stdout or ":syslog" to log to syslog`)
+	flag.StringVarP(&logLevel, "log-level", "L", "info", "Logging level")
+	flag.BoolVarP(&cfg.DryRun, "dry-run", "d", false, "Run in dry-run mode (read-only)")
+
+	flag.ErrHelp = errors.New("") // Don't print "pflag: help requested" when the user invokes the help flags
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "snap-o-matic - Automatic Exoscale Compute instance volume snapshot")
+		fmt.Fprintln(os.Stderr, "Usage:")
+		flag.PrintDefaults()
+		fmt.Fprintf(os.Stderr, `
+Supported environment variables:
+  EXOSCALE_API_ENDPOINT    Exoscale Compute API endpoint (default "https://api.exoscale.com/compute")
+  EXOSCALE_API_KEY         Exoscale API key
+  EXOSCALE_API_SECRET      Exoscale API secret
+
+API credentials file format:
+  Instead of reading Exoscale API credentials from environment variables, it
+  is possible to read those from a file formatted such as:
+
+    api_key=EXOabcdef0123456789abcdef01
+    api_secret=AbCdEfGhIjKlMnOpQrStUvWxYz-0123456789aBcDef
+`)
+	}
+
+	flag.Parse()
+
+	logLevelHandler, err := log.LvlFromString(logLevel)
+	if err != nil {
+		dieOnError("invalid value for option --log-level: %s", err)
+	}
+
+	switch logTo {
+	case "-", "":
+		logHandler = log.StdoutHandler
+
+	case ":syslog":
+		if logHandler, err = log.SyslogHandler(
+			syslog.LOG_INFO|syslog.LOG_LOCAL0,
+			"snap-o-matic",
+			log.LogfmtFormat()); err != nil {
+			dieOnError("unable to initialize syslog logging", "error", err)
+		}
+
+	default:
+		if logHandler, err = log.FileHandler(logTo, log.LogfmtFormat()); err != nil {
+			dieOnError("unable to initialize file logging", "error", err)
+		}
+	}
+
+	log.Root().SetHandler(log.LvlFilterHandler(logLevelHandler, logHandler))
+
+	exo = egoscale.NewClient(cfg.APIEndpoint, cfg.APIKey, cfg.APISecret)
+}
+
+func main() {
+	var ctx context.Context = context.Background()
+
+	if cfg.InstanceID == "" {
+		log.Debug("looking up instance ID via metadata service")
+
+		res, err := http.Get(metadataURL)
+		if err != nil {
+			dieOnError("unable to find instance ID", "error", err)
+		}
+		defer res.Body.Close()
+
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			dieOnError("unable to read metadata service request reply", "error", err)
+		}
+		cfg.InstanceID = string(body)
+	}
+
+	log.Debug("settings",
+		"api_endpoint", cfg.APIEndpoint,
+		"api_key", cfg.APIKey,
+		"api_secret", scrambleString(cfg.APISecret),
+		"instance_id", cfg.InstanceID,
+		"max_snapshots_retention", cfg.SnapshotsRetention,
+		"dry-run", cfg.DryRun,
+	)
+
+	res, err := exo.GetWithContext(ctx, &egoscale.ListVolumes{
+		VirtualMachineID: egoscale.MustParseUUID(cfg.InstanceID),
+	})
+	if err != nil {
+		dieOnError("unable to find instance storage volumes", "error", err)
+	}
+	instanceVolume := res.(*egoscale.Volume)
+
+	rotateSnapshots(ctx, instanceVolume.ID)
+	takeSnapshot(ctx, instanceVolume.ID)
+}
+
+// rotateSnapshots lists the existing instance snapshots and deletes the oldest ones in order to remain under the
+// specified retention threshold.
+func rotateSnapshots(ctx context.Context, instanceVolumeID *egoscale.UUID) {
+	res, err := exo.ListWithContext(ctx, &egoscale.ListSnapshots{VolumeID: instanceVolumeID})
+	if err != nil {
+		dieOnError("unable to list snapshots", "error", err)
+	}
+
+	sc := 0
+	for _, s := range res {
+		snapshot := s.(*egoscale.Snapshot)
+
+		// Our snapshots are tagged with a specific key, so we don't delete the snapshots a user could have taken
+		// manually
+		if !isAutosnapshot(snapshot) || strings.ToLower(snapshot.State) != "backedup" {
+			continue
+		}
+
+		log.Debug("found snapshot", "id", snapshot.ID.String())
+
+		if sc++; sc < cfg.SnapshotsRetention {
+			continue
+		}
+
+		// Snapshots are returned by the API sorted in reverse chronological order, so from the moment we exceed the
+		// maximum snapshot retention threshold we can safely delete the remaining snapshots as they are the oldest
+		if cfg.DryRun {
+			log.Info("[DRY-RUN] deleting snapshot", "id", snapshot.ID.String())
+			continue
+		}
+
+		log.Info("deleting snapshot", "id", snapshot.ID.String())
+		if err := exo.BooleanRequestWithContext(ctx, &egoscale.DeleteSnapshot{ID: snapshot.ID}); err != nil {
+			dieOnError("unable to delete snapshot", "error", err)
+		}
+	}
+}
+
+// takeSnapshot takes a new instance snapshot and tags it.
+func takeSnapshot(ctx context.Context, instanceVolumeID *egoscale.UUID) {
+	if cfg.DryRun {
+		log.Info("[DRY-RUN] creating snapshot")
+		return
+	}
+
+	log.Info("creating snapshot")
+	res, err := exo.RequestWithContext(ctx, &egoscale.CreateSnapshot{
+		VolumeID: instanceVolumeID,
+	})
+	if err != nil {
+		dieOnError("unable to create new snapshot", "error", err)
+	}
+	instanceSnapshot := res.(*egoscale.Snapshot)
+
+	if _, err := exo.RequestWithContext(ctx, &egoscale.CreateTags{
+		ResourceType: instanceSnapshot.ResourceType(),
+		ResourceIDs:  []egoscale.UUID{*(instanceSnapshot.ID)},
+		Tags:         []egoscale.ResourceTag{{Key: autosnapTag, Value: "true"}},
+	}); err != nil {
+		// Cleanup attempt: don't leave our snapshot dangling untagged
+		exo.BooleanRequestWithContext(ctx, &egoscale.DeleteSnapshot{ID: instanceSnapshot.ID}) // nolint:errcheck
+		dieOnError("unable to tag new snapshot", "error", err)
+	}
+
+	log.Debug("snapshot created successfully", "id", instanceSnapshot.ID.String())
+}
+
+// isAutosnapshot returns true if the snapshot s is an automatic snapshot.
+func isAutosnapshot(s *egoscale.Snapshot) bool {
+	for _, tag := range s.Tags {
+		if tag.Key == autosnapTag {
+			return true
+		}
+	}
+
+	return false
+}
+
+// scrambleString returns a scrambled version of s, with all characters except the first and the last replaced with
+// "*".
+func scrambleString(s string) string {
+	n := len(s)
+	switch n {
+	case 0:
+		return ""
+
+	case 1:
+		return "*"
+
+	default:
+		scrambled := make([]byte, n)
+		for i := range s {
+			if i == 0 || i == n-1 {
+				scrambled[i] = s[i]
+				continue
+			}
+			scrambled[i] = '*'
+		}
+
+		return string(scrambled)
+	}
+}
+
+func dieOnError(msg string, ctx ...interface{}) {
+	log.Error(msg, ctx...)
+	os.Exit(1)
+}
